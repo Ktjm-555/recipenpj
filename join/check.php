85d747762d587df1fb8e07e21f1a6e50ed712ebf
“Ktjm-555” <m250919kaho@gmail.com>
2022-04-04 23:51:31 +0900

    会員登録できるようになった

--- join/check.php
+++ join/check.php
@@ -11,6 +11,10 @@
     <title>登録確認画面</title>
 </head>
 <body>
+<?php
+$db = new mysqli('localhost:8889', 'root', 'root', 'recipenpj');
+$db->query
+    
     
 </body>
 </html>
--- join/index.php
+++ join/index.php
@@ -1,19 +1,48 @@
 
 <?php
-$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING)
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 
-$db = new mysqli('localhost:8889', 'root', 'root', 'recipenpj')
+$db = new mysqli('localhost:8889', 'root', 'root', 'recipenpj');
 
-$stmt = $db->prepare('insert into member(name, email, ) vale(?, ?, ?)');
-if (!$stmt){
-    die($db->error);
+// $db->query("INSERT INTO member(name, email, password) VALUES ('aa', 'bb', 'cc') ");
+
+$sql = "INSERT INTO 
+member
+(name, email, password) 
+VALUES 
+('".$name."','".$email."','".$password."')";
+
+$res = $db->query($sql);
+if ($res){
+    echo '登録できました';
+}else{
+    echo 'できていませんよ！何かがおかしいよ！';
 }
 
 
+
+// $stmt = $db->prepare('insert into member(name, email, password) value(?, ?, ?)');
+// if (!$stmt){
+//     die($db->error);
+// }
+
+// $stmt->bind_param('sss', $name, $email, $password);
+// $ret = $stmt->execute();
+
+// if ($ret):
+// echo "登録されました";
+// else:
+//     $db->error;
+// endif;
 ?>
 
 
-<!-- $passsword = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING)
-$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL) -->
+    
 
-<
+
+
+
+
+
