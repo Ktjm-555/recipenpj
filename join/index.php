8de603fbdd09ec325ad7536ecd3476e77a603775
“Ktjm-555” <m250919kaho@gmail.com>
2022-04-04 22:44:09 +0900

    ファイルの追加

--- join/index.php
+++ join/index.php
@@ -1,26 +1,19 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>会員登録画面</title>
-</head>
-<body>
-    <header>
-        <h1>会員登録<h1>
-    </header>
-    <form aciton="check.php" method="post">
-        <textarea name="name" >
-   
-</body>
-</html>
-    
+
+<?php
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING)
+
+$db = new mysqli('localhost:8889', 'root', 'root', 'recipenpj')
+
+$stmt = $db->prepare('insert into member(name, email, ) vale(?, ?, ?)');
+if (!$stmt){
+    die($db->error);
+}
 
 
-<!-- ニックネーム
-    <input type="text" name="name" size="35" maxlength="255" value=""/>
-    メールアドレス
-    <input type="text" name="email" size="35" maxlength="255" value=""/>
-    パスワード
-    <input type="password" name="password" size="10" maxlength="20" value=""/> -->
+?>
+
+
+<!-- $passsword = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING)
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL) -->
+
+<
