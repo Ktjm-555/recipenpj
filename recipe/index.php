ddbbe8140c86d105c61f5ac73999097a86017330
“Ktjm-555” <m250919kaho@gmail.com>
2022-04-06 11:36:31 +0900

    ファイル移動

---
+++ recipe/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>レシピ投稿画面</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+<div><h1>レシピ投稿</h1></div>
+<form action="" method="post" enctype="multipart/form-data">
+<p class="toukou">レシピ名</P>
+<p class="error">レシピ名を入力してください。</p>
+<p class="toukou">写真</P>
+<p class="error">写真を投稿してください。</p>
+<p class="toukou">材料</P>
+<p class="error">材料を入力してください。</p>
+<p class="toukou">作り方</P>
+<p class="error">作り方を入力してください。</p>
+
+</form>
+
+
+</body>
+
+</html>
