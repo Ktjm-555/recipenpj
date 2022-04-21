9f086e374b8ea3046a6cef4086cdcbb32f870ce8
“Ktjm-555” <m250919kaho@gmail.com>
2022-04-21 18:56:46 +0900

    ログインCSS

--- login.php
+++ login.php
@@ -50,82 +50,97 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['type'] == "1"){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="log_style.css">
+    <link rel="stylesheet" href="login_style.css">
     <title>ログイン</title>
 </head>
-
 <body>
-    <div class="login_page">
+    <div class="container">
+        <header>
+            <h1 class="title">Recipen ログイン</h1>
+            <nav class="nav">
+                <div class="button5">
+                    <form action="toppage.php" method="post" >
+                        <button type="submit"> 
+                            TOPページに戻る
+                        </button>
+                    </form>
+                </div>
+            </nav>
+        </header>    
+        <div class="main">
+            <div class="join_page">
+                <div class=join_page2>
+                    <div class="join_form">
+                        <div class=page_title>
+                            <h1>ようこそ！！</h1>
+                        </div>
 
-        <div class=page_title>
-            <h1>ログイン画面</h1>
+                        <div class="form_title">
+                            <p>会員登録まだの方はこちらへ</p>
+                        </div>
+
+                        <div class="form2">
+                            <form action="join/index.php" method="post" >
+                            <input type="hidden" name="type" value="2">
+                                <button type="submit"> 
+                                会員登録する
+                                </button>
+                            </form>
+                        </div>
+                        <form action="" method="post">
+                            <input type="hidden" name="type" value="1">
+                            <dl>
+                                <div class="form_title">
+                                    <dt>メールアドレス</dt>
+                                </div>
+
+                                <div class="form_contents">
+                                    <label class="ef">
+                                        <dd> 
+                                            <input type="text" name="email" size="35" maxlength="255" value="<?php echo h($email); ?>"/>
+                                        </dd>
+                                    </label>
+                                </div>
+
+
+                                <div class="form_title">
+                                    <dt>パスワード</dt>
+                                </div>
+
+                                <div class="form_contents">
+                                    <label class="ef">
+                                        <dd> 
+                                            <input type="password" name="password" size="35" maxlength="255" value="<?php echo h($password); ?>"/>
+                                        </dd> 
+                                    </label>
+                                </div>
+
+                            </dl>
+                            <div class="error">
+                                <?php if (isset($error['login']) && $error['login'] == 'blank'): ?>
+                                <p>*メールアドレスとパスワードを両方記入してログインしましょう！</p>
+                                <?php endif; ?>
+                            </div>
+                            
+                            <div class="error">
+                                <?php if (isset($error['login']) && $error['login'] == 'failed'): ?>
+                                <p>*ログインに失敗しました。正しく入力しまししょう！</p>
+                                <?php endif; ?>
+                            </div>
+
+                        
+                            <div class="form2">
+                                <button type="submit">ログイン</button> 
+                            </div>
+
+                        </form>
+                    </div>
+                </div>        
+            </div>
         </div>
-
-        <div class="form_title">
-            <p>会員登録まだの方はこちらへ</p>
-        </div>
-
-        <div class="form2">
-            <form action="join/index.php" method="post" >
-            <input type="hidden" name="type" value="2">
-                <button type="submit"> 
-                会員登録する
-                </button>
-            </form>
-        </div>
-
-        
-
-        <form action="" method="post">
-            <input type="hidden" name="type" value="1">
-            <dl>
-                <div class="form_title">
-                    <dt>メールアドレス</dt>
-                </div>
-
-                <div class="form_contents">
-                    <label class="ef">
-                        <dd> 
-                            <input type="text" name="email" size="35" maxlength="255" value="<?php echo h($email); ?>"/>
-                        </dd>
-                    </label>
-                </div>
-
-
-                <div class="form_title">
-                    <dt>パスワード</dt>
-                </div>
-
-                <div class="form_contents">
-                    <label class="ef">
-                        <dd> 
-                            <input type="password" name="password" size="35" maxlength="255" value="<?php echo h($password); ?>"/>
-                        </dd> 
-                    </label>
-                </div>
-
-            </dl>
-            <div class="error">
-                <?php if (isset($error['login']) && $error['login'] == 'blank'): ?>
-                <p>*メールアドレスとパスワードを両方記入してログインしましょう！</p>
-                <?php endif; ?>
-            </div>
-            <div class="error">
-                <?php if (isset($error['login']) && $error['login'] == 'failed'): ?>
-                <p>*ログインに失敗しました。正しく入力しまししょう！</p>
-                <?php endif; ?>
-            </div>
-
-           
-            <div class="form2">
-                <button type="submit">ログイン</button> 
-            </div>
-        </form>
-            <div class="form3">
-                <form action="toppage.php" method="post" >
-                    <button type="submit">TOPページに戻る</button>
-                </form>
-            </div>
+        <footer>
+        2022 @recipenpj
+        </footer>
     </div>
 </body>
 </html>
