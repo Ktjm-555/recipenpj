53755555c4bb776401711127cd599b78fcff324d
“Ktjm-555” <m250919kaho@gmail.com>
2022-04-21 17:14:28 +0900

    投稿画面CSS確認

--- recipe/check.php
+++ recipe/check.php
@@ -10,8 +10,6 @@ if (isset($_SESSION['form'])){
     exit();
 }
 
-
- 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     $db = new mysqli('localhost:8889', 'root', 'root', 'recipenpj'); 
@@ -33,11 +31,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         echo 'できていませんよ！何かがおかしいよ！'; 
         // var_dump($form['recipename']);
         // exit();
-    }
-    
-
+    }   
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -50,56 +45,72 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     <title>投稿確認画面</title>
 </head>
 <body>
-<div class="container">
-<!-- <div class="recipe_check_page"> -->
-    
-    <div class="recipe_form">
-        <div class=page_title>
-            <h1>確認画面</h1>
-        </div>
-        <form action="" method="post" enctype="multipart/form-data">
-            <div class="form_title">
-                <dt>レシピ名</dt>
-            </div>
+    <div class="container"> 
+        <header>
+            <h1 class="title">Recipen 投稿画面</h1>
+			<nav class="nav">
+				<div class="button5">
+					<form action="../toppage.php" method="post" >
+						<button type="submit"> 
+							TOPページに戻る
+						</button>
+					</form>
+				</div>
+			</nav>
+        </header>
+        <div class="main">
+            <div class=join_page2>  
+                <div class="recipe_form">
+                    <div class=page_title>
+                        <h1>確認画面</h1>
+                    </div>
+                    
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div class="form_title2">
+                            <dt>レシピ名</dt>
+                        </div>
+                        <dl>
+                        <div class="form_title2">
+                            <dd><?php echo h($form['recipe']); ?></dd>
+                        </div>
 
-        <dl>
-            <div class="form">
-                <dd><?php echo h($form['recipe']); ?></dd>
-            </div>
+                        <div class="form_title2">
+                            <dt>写真</dt>
+                        </div>  
+                        
+                        <div class="form_title2">
+                            <dd>
+                                <img src="../recipe_picture/<?php echo h($form['image']); ?>">
+                            </dd>
+                        </div>  
 
-            <div class="form_title">
-                <dt>写真</dt>
-            </div>  
-            
-            <div class="form">
-                <dd>
-                    <img src="../recipe_picture/<?php echo h($form['image']); ?>">
-                </dd>
-            </div>  
+                        <div class="form_title2">
+                            <dt>材料</dt>
+                        </div>
 
-            <div class="form_title">
-                <dt>材料</dt>
-            </div>
+                        <div class="form_title2">
+                            <dd><?php echo h($form['foodstuffs']); ?></dd>
+                        </div>
 
-            <div class="form">
-                <dd><?php echo h($form['foodstuffs']); ?></dd>
-            </div>
+                        <div class="form_title2">
+                            <dt>作り方</dt>
+                        </div>
 
-            <div class="form_title">
-                <dt>作り方</dt>
-            </div>
+                        <div class="form_title2">
+                            <dd><?php echo h($form['recipe']); ?></dd>
+                        </div>
+                        </dl>
 
-            <div class="form">
-                <dd><?php echo h($form['recipe']); ?></dd>
+                        <div class="form2">
+                        <div><button type="submit">投稿する</button></div>
+                        </div>
+                    </form>
+                </div>
             </div>
-            </dl>
-            <div class="form3">
-            <div><button type="submit">投稿する</button></div>
-            </div>
-        </form>
+        </div>   
+        <footer>
+            @2022 recipenpj
+        </footer>
     </div>
-<!-- </div> -->
-</div>
-
 </body>
 </html>
